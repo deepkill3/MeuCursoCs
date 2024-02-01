b6a1bacf7f6366dff44b700a621ae7a73662325d
deepkill3 <meunomeeasuamorte@gmail.com>
2024-01-31 22:50:49 -0300

    Meu curso de C#

---
+++ Aula07/aula7.cs
@@ -0,0 +1,20 @@
+using System.Reflection.Metadata;
+/*
+Diferente de uma variável, uma constante não pode ter seu valor alterado ao decorrer do programa.
+
+usando a palavra reservada "const", para dizer que isso é uma constante, e seu valor inicial será sempre o
+mesmo que foi setado.
+
+                                        --+Exemplo+--
+
+const
+*/
+
+using System;
+
+class aula7 {
+    static void Main() {
+        public
+
+    }
+}
