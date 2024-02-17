15d228c7bc3ef9a19bea8d9fe65aeeb65a903f32
Mateus Santos <meunomeeasuamorte@gmail.com>
2024-02-17 17:10:13 -0300

    Curso C#

---
+++ Aula12/aula12.cs
@@ -0,0 +1,14 @@
+using System;
+class aula12
+{
+    static void Main(){
+        int num = 10;
+        if (num == 10) {
+            Console.WriteLine("Verdadeiro");
+        }
+
+        else {
+            Console.WriteLine("Falso");
+        }
+    }
+}
