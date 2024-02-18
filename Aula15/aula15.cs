558bcb4628d3919af9f631d1023d08a2f781f35a
Mateus Santos <meunomeeasuamorte@gmail.com>
2024-02-18 14:11:49 -0300

    Curso C#

--- Aula15/aula15.cs
+++ Aula15/aula15.cs
@@ -16,26 +16,27 @@ class aula15{
         escolha = char.Parse(Console.ReadLine());
         switch(escolha){
             case 'a':
+            case 'A':
                 tempo = 50;
                 break;
             case 'c':
+            case 'C':
                 tempo = 480;
                 break;
             case 'o':
+            case 'O':
                 tempo = 660;
                 break;
             default:
                 tempo=-1;
                 break;
-
+        }
         if(tempo <0){
-            Console.WriteLine("Transporte indisponível")
+            Console.WriteLine("Transporte indisponível");
         }
         else {
-            Console.WriteLine("Para o transporte escolhido, o tempo é: {0} minutos")
-
+            Console.WriteLine("Para o transporte escolhido, o tempo é: {0} minutos", tempo);
         }
 
-        }
     }
 }
---
+++ Aula16/aula16.cs
@@ -0,0 +1,6 @@
+using System;
+class aula16 {
+    static void Main(){
+        
+    }
+}
