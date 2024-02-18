5eec8f03b7ae606bde2631686b16420d57f6a484
Mateus Santos <meunomeeasuamorte@gmail.com>
2024-02-17 22:24:13 -0300

    Curso C#

---
+++ Aula15/aula15.cs
@@ -0,0 +1,41 @@
+/*
+                                    Switch Case - Aprendendo sobre
+
+Laço condicional com propósito diferente do if... 
+*/
+
+using System;
+
+class aula15{
+    static void Main(){
+        int tempo=0;
+        char escolha;
+
+        Console.WriteLine("De Belo Horizonte/MG a Vitória/ES");
+        Console.WriteLine("Escolha o transporte:[a]Avião | [c]Carro | [o]Ônibus");
+        escolha = char.Parse(Console.ReadLine());
+        switch(escolha){
+            case 'a':
+                tempo = 50;
+                break;
+            case 'c':
+                tempo = 480;
+                break;
+            case 'o':
+                tempo = 660;
+                break;
+            default:
+                tempo=-1;
+                break;
+
+        if(tempo <0){
+            Console.WriteLine("Transporte indisponível")
+        }
+        else {
+            Console.WriteLine("Para o transporte escolhido, o tempo é: {0} minutos")
+
+        }
+
+        }
+    }
+}
