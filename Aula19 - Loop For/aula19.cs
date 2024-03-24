336d84666382a202a2f98948251be23eb31d3d82
deepkill3 <meunomeeasuamorte@gmail.com>
2024-03-24 18:18:00 -0300

    Curso C#

--- Aula19 - Loop For/aula19.cs
+++ Aula19 - Loop For/aula19.cs
@@ -2,8 +2,12 @@ using System;
 
 class aula19{
     static void Main(){
-        for (e1;e2;e3){
-            
-        }
+        //Criando uma lista de números
+            int[] numeros = {1,2,3,4,5};
+
+            //Usando o for para percorrer a lista e exibir cada elemento
+            for (int i = 0; i < numeros.Length; i++){
+                Console.WriteLine("Elemento {0}: {1}",i,numeros[1]);
+            }
     }
 }
