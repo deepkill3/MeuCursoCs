e869c45515430454bbb62da62f41fa13e9d29ae4
deepkill3 <meunomeeasuamorte@gmail.com>
2024-03-24 19:04:12 -0300

    Curso C#

--- Aula19 - Loop For/aula19.cs
+++ Aula19 - Loop For/aula19.cs
@@ -3,11 +3,15 @@ using System;
 class aula19{
     static void Main(){
         //Criando uma lista de números
-            int[] numeros = {1,2,3,4,5};
+        int[]num = new int[10];
 
-            //Usando o for para percorrer a lista e exibir cada elemento
-            for (int i = 0; i < numeros.Length; i++){
-                Console.WriteLine("Elemento {0}: {1}",i,numeros[1]);
-            }
+
+        //Usando o for para percorrer a lista e exibir cada elemento
+        for(int i = 0; i < num.Length; i++) {
+            num[i] = i;
+        }
+        for(int i = 0; i < num.Length; i++) {
+            Console.WriteLine("Valor de Num na pos{0}: {1}", i, num[i]);
+        }
     }
 }
--- Aula20 - Loop While/aula20.cs
+++ Aula20 - Loop While/aula20.cs
@@ -2,6 +2,16 @@ using System;
 
 class Aula20 {
     static void Main() {
-        
+
+        int[] num=new int[10];
+
+        int i=0;
+        while(i<num.Length){
+            /*Console.WriteLine("CFB Cursos");*/
+            num[i]=0;
+            Console.WriteLine(num[i]);
+            i++;
+        }
+        Console.WriteLine("Fim do loop");
     }
 }
