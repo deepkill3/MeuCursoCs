967fbd5f7938b0f2b9fe348ca9b6c3924ba8a424
deepkill3 <meunomeeasuamorte@gmail.com>
2024-03-27 21:50:49 -0300

    Curso C#

---
+++ Aula24 - Métodos/aula24.cs
@@ -0,0 +1,16 @@
+/*
+                            --+Métodos+--  
+
+
+
+=> São conjunto de instruções que pode ser chamado a qualquer momento em nosso programa.
+
+*/
+
+using System;
+
+class Aula24 {
+    static void Main() {
+
+    }
+}
